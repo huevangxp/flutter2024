73341e5c38dc49b946db18be7af963ce64fed84b
huevangxp <huevang933@gmail.comcd>
2024-05-14 15:14:31 +0700

    add cart shopping

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -30,57 +30,102 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page', style: TextStyle(color: Colors.white),),
+        title: const Text(
+          'Home Page',
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.teal,
       ),
       body: Column(
         children: <Widget>[
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: ((context)=> VideoPlayer())));
-          }, child:const Text('Go to Video')),
-            Center(
-            child: Container(
-              margin:const EdgeInsets.only(top:2),
-              padding:const EdgeInsets.only(top:5, bottom: 5),
-              color: const Color.fromARGB(255, 48, 102, 97),
-              child: const Row(
+          // ElevatedButton(onPressed: (){
+          //   Navigator.push(context, MaterialPageRoute(builder: ((context)=> VideoPlayer())));
+          // }, child:const Text('Go to Video')),
+          Center(
+              child: Container(
+            margin: const EdgeInsets.only(top: 2),
+            padding: const EdgeInsets.only(top: 5, bottom: 5),
+            color: const Color.fromARGB(255, 48, 102, 97),
+            child: const Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text("Title", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
-                Text("Description", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
-                Text("Price", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
+                Text(
+                  "Title",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text("Description",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+                Text("Price",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
               ],
             ),
-            )
-          ),
+          )),
           Expanded(
             child: ListView.builder(
               itemCount: items.length,
               itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child:Image.network('https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',width: 100, height: 100,),
-                      ),
-                    const  Flexible(
-                        flex: 2,
-                        child: Text(
-                          'The meaning of DESCRIPTION is an act of describing; specifically: discourse intended to give a mental image of something experienced.',
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
+                return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: ((context) => VideoPlayer())));
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        border: const Border(
+                          left: BorderSide(
+                            color: Colors.green,
+                            width: 3,
+                          ),
+                          right: BorderSide(
+                            color: Colors.green,
+                            width: 3,
+                          ),
                         ),
                       ),
-                     const Flexible(
-                        flex: 1,
-                        child: Text('300000'),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Flexible(
+                              flex: 1,
+                              child: Image.network(
+                                'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
+                                width: 100,
+                                height: 100,
+                              ),
+                            ),
+                            const Flexible(
+                              flex: 2,
+                              child: Text(
+                                'The meaning of DESCRIPTION is an act of describing; specifically: discourse intended to give a mental image of something experienced.',
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+                            ),
+                            const Flexible(
+                              flex: 1,
+                              child: Text('300000'),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
-                );
+                    ));
               },
             ),
           ),
--- lib/pages/video/videoplayer.dart
+++ lib/pages/video/videoplayer.dart
@@ -1,4 +1,5 @@
 import 'package:flick_video_player/flick_video_player.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -10,6 +11,9 @@ class VideoPlayer extends StatefulWidget {
 }
 
 class _VideoPlayerState extends State<VideoPlayer> {
+
+var amout = 0;
+
   late FlickManager flickManager;
 
   @override
@@ -30,10 +34,93 @@ class _VideoPlayerState extends State<VideoPlayer> {
         ),
         backgroundColor: Colors.teal,
       ),
-      body: Center(
-        child:AspectRatio(aspectRatio: 16/10,
-        child: FlickVideoPlayer(flickManager: flickManager),),
-      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        AspectRatio(
+          aspectRatio: 16 / 10,
+          child: FlickVideoPlayer(flickManager: flickManager),
+        ),
+        Container(
+          padding: EdgeInsets.all(5),
+          child: Text('Title', style: TextStyle(fontSize: 20),),
+        ),
+        Container(
+          padding: EdgeInsets.all(5),
+          child: Text('The meaning of DESCRIPTION is an act of describing; specifically : discourse intended to give a mental image of something experienced.',),
+        ),
+        Container(
+          padding: EdgeInsets.all(5),
+          child: Text('price : 30.000.000 kip',),
+        ),
+        Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+              crossAxisAlignment:CrossAxisAlignment.start,
+
+                    children: [
+                       Image.network('https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg', width: 200, height: 200,)
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            padding:const EdgeInsets.all(20),
+            decoration: BoxDecoration( 
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.teal.withOpacity(0.1)
+            ),
+            child:  Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              // Icon(Icons.add_circle),
+              Container(
+                child: ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    if(amout >= 1){
+                      amout --;
+                    }
+                  });
+                }, 
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent)),
+                child: Icon(Icons.remove_shopping_cart_sharp),
+                )
+              ),
+              Text('$amout', style: TextStyle(fontSize: 18),),
+              // 
+              Container(
+                child: ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    amout ++;
+                  });
+                }, 
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent)),
+                child: Icon(Icons.add_shopping_cart ),
+                ),
+              ),
+              Container(
+                child: ElevatedButton.icon(
+                onPressed: (){
+                  
+                }, 
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent)),
+                label: Text('Add Cart'), 
+                icon: Icon(Icons.shopping_cart_checkout),),
+              )
+            ],),
+          )
+          
+      ]),
     );
   }
 }
