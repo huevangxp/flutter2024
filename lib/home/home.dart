a1339ef3d0c6a7d032305ed922fccc57f5a76a43
huevangxp <huevang933@gmail.comcd>
2024-05-14 10:35:20 +0700

    new project

---
+++ lib/home/home.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> items = List<String>.generate(40, (i) => "Item $i");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.teal,
+      ),
+      body: Column(
+        children: <Widget>[
+            Center(
+            child: Container(
+              margin: EdgeInsets.only(top:2),
+              padding: EdgeInsets.only(top:5, bottom: 5),
+              color: const Color.fromARGB(255, 48, 102, 97),
+              child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text("Title", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
+                Text("Description", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
+                Text("Price", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
+              ],
+            ),
+            )
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child:Image.network('https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',width: 100, height: 100,),
+                      ),
+                      Flexible(
+                        flex: 2,
+                        child: Text(
+                          'The meaning of DESCRIPTION is an act of describing; specifically: discourse intended to give a mental image of something experienced.',
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Text('300000'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
